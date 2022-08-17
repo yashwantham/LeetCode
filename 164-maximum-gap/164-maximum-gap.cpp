9486e07f4ae7c12a1003891c542bd4ccf68f3c61
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-18 00:00:23 +0530

    Time: 364 ms (32.52%), Space: 82 MB (49.76%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        if(nums.size() == 1) {return 0;}
         sort(nums.begin(), nums.end());
-        int maxDiff = INT_MIN;
-        for(int i = 0; i < nums.size() - 1; i++) {
-            int diff = abs(nums[i] - nums[i + 1]);
+        int maxDiff = 0;
+        for(int i = 1; i < nums.size(); i++) {
+            int diff = nums[i] - nums[i - 1];
             maxDiff = max(diff, maxDiff);
         }
         return maxDiff;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int maximumGap(vector<int>& nums) {
+//         if(nums.size() == 1) {return 0;}
+//         sort(nums.begin(), nums.end());
+//         int maxDiff = INT_MIN;
+//         for(int i = 0; i < nums.size() - 1; i++) {
+//             int diff = abs(nums[i] - nums[i + 1]);
+//             maxDiff = max(diff, maxDiff);
+//         }
+//         return maxDiff;
+//     }
+// };
