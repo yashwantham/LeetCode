08d64c49388374b744d0bed352b6a9f5123ab515
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-18 12:51:38 +0530

    Time: 139 ms (32.02%), Space: 48.4 MB (45.07%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,10 +3,14 @@ public:
     bool canJump(vector<int>& nums) {
         int maxReachableIndex = 0;
         for(int i = 0; i < nums.size() && i <= maxReachableIndex; i++) {
-            int jump = i + nums[i];
-            maxReachableIndex = max(jump, maxReachableIndex);
+            //int jump = i + nums[i];
+            maxReachableIndex = max(i + nums[i], maxReachableIndex);
         }
         return maxReachableIndex >= nums.size() - 1;
-    } 
-    
+    }   
 };
+
+
+
+// Resources
+// https://www.youtube.com/watch?v=muDPTDrpS28
