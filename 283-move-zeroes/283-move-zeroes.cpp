d8d8a8b5808cfd3742a20c9332e3502e4a8c3223
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-19 11:53:19 +0530

    Time: 42 ms (41.13%), Space: 19.3 MB (13.44%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,10 +3,23 @@ public:
     void moveZeroes(vector<int>& nums) {
         int nonZi = 0;
         for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != 0) {
+            if(nums[i]) {
                 swap(nums[nonZi++], nums[i]);
-                // nonZi++;
             }
         }
     }
 };
+
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int nonZi = 0;
+//         for(int i = 0; i < nums.size(); i++) {
+//             if(nums[i] != 0) {
+//                 swap(nums[nonZi], nums[i]);
+//                 nonZi++;
+//             }
+//         }
+//     }
+// };
