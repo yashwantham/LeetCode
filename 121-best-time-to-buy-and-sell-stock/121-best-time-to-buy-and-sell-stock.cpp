7836b4aeedc903bbf0dffee17250669a83f594a2
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-20 11:05:14 +0530

    Time: 223 ms (39.60%), Space: 93.4 MB (11.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,13 @@ public:
         int mini = INT_MAX, maxPro = 0;
         for(int i = 0; i < prices.size(); i++) {
             mini = min(mini, prices[i]);
-            int profit = prices[i] - mini;
-            maxPro = max(maxPro, profit);
+            maxPro = max(maxPro, prices[i] - mini);
         }
         return maxPro;
     }
 };
+
+
+
+// Resources
+// https://www.youtube.com/watch?v=eMSfBgbiEjk
