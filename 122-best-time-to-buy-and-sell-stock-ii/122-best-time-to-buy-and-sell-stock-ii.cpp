cd05a22e9c4f02915cd7668f8fbc2be65e055216
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-21 16:56:26 +0530

    Time: 14 ms (41.12%), Space: 12.9 MB (90.07%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,7 +4,7 @@ public:
         int profit = 0;
         for(int i = 1; i < prices.size(); i++) {
             if(prices[i - 1] < prices[i]) {
-                profit = profit + (prices[i] - prices[i - 1]);
+                profit += (prices[i] - prices[i - 1]);
             }
         }
         return profit;
