49214203eac3f78a84808fb598371bed37e845b5
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-22 00:28:10 +0530

    Time: 21 ms (56.15%), Space: 5.7 MB (91.05%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        bool pali = false;
-        
-        if(x < 0) { return pali; }
-        
-        
-        long start = x, reversed = 0;
+    bool isPalindrome(int x) {   
+        if(x < 0) { return false; } 
+        int start = x;
+        long reversed = 0;
         while(start > 0) {
-            int rem = start % 10;
-            reversed = (reversed * 10) + rem;
+            reversed = (reversed * 10) + start % 10;
             start = start / 10;
-        }
-        
-        if(reversed == x) { 
-            pali = true; 
-        }
-        else {  
-            pali = false; 
-        }
-        return pali;
+        }      
+        if(reversed == x) { return true; }
+        else { return false; }
     }
 };
