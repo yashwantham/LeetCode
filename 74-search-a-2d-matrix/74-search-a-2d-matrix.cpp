d30f4449a94d86e8d3dfbcf5b8871803497814c1
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-29 14:34:06 +0530

    Time: 8 ms (43.66%), Space: 9.4 MB (95.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,49 +1,12 @@
-// Optimal
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        if(matrix.size() == 0) { return false; }
-        
-        int m = matrix.size(), n = matrix[0].size();
-        int start = 0, end = (m * n) - 1;
-        while(start <= end) {
-            int mid = (start + end)/2;
-            if(matrix[mid / n][mid % n] == target) { return true; }
-            if(matrix[mid / n][mid % n] > target) { end = mid - 1; }
-            else { start = mid + 1; }
+        int col = matrix[0].size();
+        for(int r = 0; r < matrix.size(); r++) {
+            for(int c = 0; c < col; c++) {
+                if(matrix[r][c] == target) { return true; }
+            }
         }
         return false;
     }
 };
-
-
-// Better 
-// link:  https://leetcode.com/problems/search-a-2d-matrix/discuss/1895837/C%2B%2B-BINARY-SEARCH-TREE-(**)-Explained-with-IMG
-// class Solution {
-// public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int r = 0, c = matrix[0].size() - 1;
-//         while(r < matrix.size() && c > -1) {
-//             if(matrix[r][c] == target) { return true; }
-//             if(target > matrix[r][c]) { r++; }
-//             else { c--; }
-//         }
-//         return false;
-//     }
-// };
-
-
-//Brute Force
-// class Solution {
-// public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int col = matrix[0].size();
-//         for(int r = 0; r < matrix.size(); r++) {
-//             for(int c = 0; c < col; c++) {
-//                 if(matrix[r][c] == target) { return true; }
-//             }
-//         }
-//         return false;
-//     }
-// };
