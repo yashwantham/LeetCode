600e9fc0f343e35fbca20b262583e2f555ec9077
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-29 23:05:13 +0530

    Time: 412 ms (23.39%), Space: 124 MB (66.99%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> newArray(nums.size());
-        int pi = 0, ni = 1;
-        for(int num: nums) {
-            if(num > 0) {
-                newArray[pi] = num;
-                pi += 2;
-            }
-            else {
-                newArray[ni] = num;
-                ni += 2;
-            }
-        }
-        return newArray;
-    }
-};
-
-
-/*
-
-
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> newArray(nums.size(), 0);
         if(nums.size() == 1) { return nums; }
         int pi = 0, ni = 1;
@@ -41,5 +18,27 @@ public:
     }
 };
 
+
+
+/*
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> newArray(nums.size());
+        int pi = 0, ni = 1;
+        for(int num: nums) {
+            if(num > 0) {
+                newArray[pi] = num;
+                pi += 2;
+            }
+            else {
+                newArray[ni] = num;
+                ni += 2;
+            }
+        }
+        return newArray;
+    }
+};
 
 */
