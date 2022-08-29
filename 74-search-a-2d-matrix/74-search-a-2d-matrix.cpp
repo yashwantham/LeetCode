42a37327467231d735def3231043c014fb12eb68
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-29 14:31:23 +0530

    Time: 12 ms (15.18%), Space: 9.5 MB (78.48%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -8,8 +8,8 @@ public:
         int start = 0, end = (m * n) - 1;
         while(start <= end) {
             int mid = (start + end)/2;
-            if((matrix[mid / n][mid % n]) == target) { return true; }
-            if((matrix[mid / n][mid % n]) > target) { end = mid - 1; }
+            if(matrix[mid / n][mid % n] == target) { return true; }
+            if(matrix[mid / n][mid % n] > target) { end = mid - 1; }
             else { start = mid + 1; }
         }
         return false;
