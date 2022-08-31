f7ad974179261d3247a7651cda3542396b5e0ddc
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-08-31 17:33:35 +0530

    Time: 14 ms (81.07%), Space: 10.7 MB (43.92%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -3,13 +3,11 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         
         int m = matrix.size(), n = matrix[0].size();
-        
         vector<vector<int>> result(n, vector<int> (m));
         
         for(int i = 0; i < m * n; i++) {
             result[i % n][i / n] = matrix[i / n][i % n]; 
          }
-        
         return result;
     }
 };
