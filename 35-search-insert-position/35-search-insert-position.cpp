4764648a67ba2d70f7bf00c7422cf3a4c6e846f1
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-09-03 01:28:22 +0530

    Time: 7 ms (62.04%), Space: 9.7 MB (71.97%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i;
-        for(i = 0; i < nums.size(); i++) {
-            if(nums[i] == target || nums[i] > target) { return i; }
+        int s = 0, e = nums.size() - 1;
+        while(s <= e) {
+            int mid = (s + e) / 2;
+            if(nums[mid] == target) { return mid; }
+            else if(nums[mid] < target) { s = mid + 1; }
+            else { e = mid - 1; }
         }
-        return i;
+        return s;
     }
 };
+
+
+// // Brute Force
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int i;
+//         for(i = 0; i < nums.size(); i++) {
+//             if(nums[i] == target || nums[i] > target) { return i; }
+//         }
+//         return i;
+//     }
+// };
