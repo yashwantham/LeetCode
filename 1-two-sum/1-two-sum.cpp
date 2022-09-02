9daf9350c6feba7e1fc897ec7a57f9d1481cee71
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-09-03 00:15:41 +0530

    Time: 17 ms (73.90%), Space: 10.6 MB (56.62%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -10,19 +10,18 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<pair<int, int>> arr(n);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             arr[i] = make_pair(nums[i], i);
         }
-        sort(arr.begin(), arr.end()); // Sort arr in increasing order by their values.
-        int left = 0, right = n- 1;
-        while (left < right) {
-            int sum2 = arr[left].first + arr[right].first;
-            if (sum2 == target) 
-                return {arr[left].second, arr[right].second};
-            if (sum2 > target)
-                right -= 1; // Try to decrease sum2
-            else
-                left += 1; // Try to increase sum2
+        sort(arr.begin(), arr.end()); // Sorted by their values.
+        int lo = 0, hi = n- 1;
+        while (lo < hi) {
+            int sum2 = arr[lo].first + arr[hi].first;
+            if (sum2 == target) {
+                return {arr[lo].second, arr[hi].second};
+            }
+            if (sum2 > target) { hi--; } 
+            else { lo++; } 
         }
         return {};
     }
