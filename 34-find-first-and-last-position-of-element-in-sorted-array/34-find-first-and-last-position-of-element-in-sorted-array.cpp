28019364e5141e606ddef2875e10532efe4ec20a
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-09-03 18:24:35 +0530

    Time: 23 ms (7.58%), Space: 13.7 MB (17.94%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,53 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> positions(2, -1);
-        bool first = true;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == target && first) {
-                positions[0] = i; 
-                first = false;
+        vector<int> result(2, -1);
+        
+        // Finding the first occurance
+        int start = 0, end = nums.size() - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) {
+                result[0] = mid;
+                end = mid - 1;
             }
-            if(nums[i] == target && first == false) {
-                positions[1] = i;
-            }
+            else if(nums[mid] < target) { start = mid + 1; }
+            else { end = mid - 1; }
         }
-        return positions;
+        
+        // Finding the second occurance
+        start = 0, end = nums.size() - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) {
+                result[1] = mid;
+                start = mid + 1;
+            }
+            else if(nums[mid] < target) { start = mid + 1; }
+            else { end = mid - 1; }
+        }
+        
+        return result;
     }
 };
+
+
+
+// // Brute Force
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int> positions(2, -1);
+//         bool first = true;
+//         for(int i = 0; i < nums.size(); i++) {
+//             if(nums[i] == target && first) {
+//                 positions[0] = i; 
+//                 first = false;
+//             }
+//             if(nums[i] == target && first == false) {
+//                 positions[1] = i;
+//             }
+//         }
+//         return positions;
+//     }
+// };
