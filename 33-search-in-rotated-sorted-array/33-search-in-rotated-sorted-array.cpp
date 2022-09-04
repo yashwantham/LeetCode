926bde3b3880aeebaf3b67ce51354843340cc47a
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-09-04 13:24:43 +0530

    Time: 10 ms (26.57%), Space: 11.2 MB (29.40%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) { return mid; }
+            
+            // check if left half is sorted
+            if(nums[start] <= nums[mid]) {
+                if(target >= nums[start] && target <= nums[mid]) { end = mid - 1; }
+                else { start = mid + 1; }
+            }
+            
+            // right half is sorted
+            else {
+                if(target >= nums[mid] && target <= nums[end]) { start = mid + 1; }
+                else { end = mid - 1; }
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
+
+// Brute force should be linear search
