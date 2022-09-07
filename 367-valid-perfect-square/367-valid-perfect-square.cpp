d82c702365da9920f6834c7c84326670876882ad
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-09-07 23:26:55 +0530

    Time: 5 ms (16.64%), Space: 5.9 MB (17.33%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,17 +1,17 @@
 
-// class Solution {
-// public:
-//     bool isPerfectSquare(int num) {
-//         int start = 1, end = num;
-//         while(start <= end) {
-//             long mid = start + (end - start) / 2;
-//             if(mid * mid == num) { return true; }
-//             else if(mid * mid < num) { start = mid + 1; }
-//             else { end = mid - 1; }
-//         }
-//         return false;
-//     }
-// };
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int start = 1, end = num;
+        while(start <= end) {
+            long mid = start + (end - start) / 2;
+            if(mid * mid == num) { return true; }
+            else if(mid * mid < num) { start = mid + 1; }
+            else { end = mid - 1; }
+        }
+        return false;
+    }
+};
 
 
 // Unique
@@ -24,16 +24,16 @@
 // 1 + 3 + 5 + 7 = 16
 // .... and so on
 
-class Solution {
-public:
-    bool isPerfectSquare(int num) {
-        int i = 1;
-        long sum = 0;
-        while(sum < num) {
-            sum += i;
-            if(sum == num) { return true; }
-            i += 2;
-        }
-        return false;
-    }
-};
+// class Solution {
+// public:
+//     bool isPerfectSquare(int num) {
+//         int i = 1;
+//         long sum = 0;
+//         while(sum < num) {
+//             sum += i;
+//             if(sum == num) { return true; }
+//             i += 2;
+//         }
+//         return false;
+//     }
+// };
