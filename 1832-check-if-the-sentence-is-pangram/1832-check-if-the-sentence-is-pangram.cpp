2e169ff4dcb71e24b2d14a59866b19129ab16e77
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-10-17 13:26:19 +0530

    Time: 4 ms (65.37%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        int freq[128] = {0};
+        int arr[26] = {0};
         
-        for(int i = 0; sentence[i] != '\0'; i++) { freq[sentence[i]]++; }
+        for(char ch: sentence) { arr[ch - 'a']++; }
         
-        for(int i = 97; i <= 122; i++) {
-            if(freq[i] == 0 ) { return false; }
+        for(int i = 0; i < 26; i++) {
+            if(arr[i] == 0) { return false; }
         }
         
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool checkIfPangram(string sentence) {
+//         int freq[128] = {0};
+        
+//         for(int i = 0; sentence[i] != '\0'; i++) { freq[sentence[i]]++; }
+        
+//         for(int i = 97; i <= 122; i++) {
+//             if(freq[i] == 0 ) { return false; }
+//         }
+        
+//         return true;
+//     }
+// };
+
+
+// Future reference:  https://leetcode.com/problems/check-if-the-sentence-is-pangram/discuss/1380853/Easy-3-approaches-with-explanation-or-C%2B%2B-or-100-time-or-One-liner
