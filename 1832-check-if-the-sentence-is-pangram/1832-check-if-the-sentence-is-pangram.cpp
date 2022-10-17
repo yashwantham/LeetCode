4c7f133fc17419e624b7b4f75205554019c5f76b
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-10-17 12:39:25 +0530

    Time: 9 ms (21.04%), Space: 6.5 MB (55.40%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int freq[256] = {0};
+        
+        for(int i = 0; sentence[i] != '\0'; i++) { freq[sentence[i]]++; }
+        
+        for(int i = 97; i <= 122; i++) {
+            if(freq[i] == 0 ) { return false; }
+        }
+        
+        return true;
+    }
+};
