9a97d6b11c85081626f38d5328959e51b74b457c
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-10-22 21:08:02 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.12%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n == 0) { return 1; }
-        // double ans = pow(x, n);
-        return pow(x, n);  
+        return pow(x, n); 
     }
 };
+
+// Brute force 1
+// class Solution {
+// public:
+//     double myPow(double x, int n) {       
+//         if(n == 0) { return 1; }
+//         return pow(x, n);  
+//     }
+// };
