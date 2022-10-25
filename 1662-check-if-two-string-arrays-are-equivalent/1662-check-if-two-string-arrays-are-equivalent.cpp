0eb576f8bd8a473b2bc94b4bdaf72d101d9aba76
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-10-25 08:34:52 +0530

    Time: 16 ms (11.45%), Space: 11.6 MB (27.47%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        int i = 0, j = 0; // word pointers
-        int m = 0, n = 0; // char pointers
-        
-        while (i < word1.size() and j < word2.size()) {            
-            if (word1[i][m++] != word2[j][n++])
-                return false;
-            
-            if (m >= word1[i].size())
-                i++, m = 0;
-            
-            if (n >= word2[j].size())
-                j++, n = 0;
-        }
-        
-        return i == word1.size() and j == word2.size();
+        string a, b;
+        for(string ch: word1) { a += ch; }
+        for(string ch: word2) { b += ch; }
+        return a == b;
+        // if(!a.compare(b)) { return true; }
+        // return false;
     }
 };
 
-// #include <cstring>
 // class Solution {
 // public:
 //     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-//         string a, b;
-//         for(string ch: word1) { a += ch; }
-//         for(string ch: word2) { b += ch; }
-//         return a == b;
-//         // if(!a.compare(b)) { return true; }
-//         // return false;
+//         int i = 0, j = 0; // word pointers
+//         int m = 0, n = 0; // char pointers
+        
+//         while (i < word1.size() and j < word2.size()) {            
+//             if (word1[i][m++] != word2[j][n++])
+//                 return false;
+            
+//             if (m >= word1[i].size())
+//                 i++, m = 0;
+            
+//             if (n >= word2[j].size())
+//                 j++, n = 0;
+//         }
+        
+//         return i == word1.size() and j == word2.size();
 //     }
 // };
+
