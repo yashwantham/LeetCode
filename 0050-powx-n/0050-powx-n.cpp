76e6a60bdf643ad3777854f1c63463c8abef4570
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-10-25 09:24:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long nn = n;
-        if(n < 0) { nn = -1 * nn; }
-        double ans = 1.0;
-        while(nn) {
-            if(nn % 2) {
-                ans = ans * x;
-                nn = nn - 1;
-            }
-            else {
-                x = x * x;
-                nn = nn / 2;
-            }
+        if(n == 0){ return 1; }
+        if (n < 0) { 
+            n = abs(n);
+            // n = -1 * n;  this will give error
+            x = 1/x;
         }
-        if(n < 0) { ans = 1.0 / ans; }
-        return ans;
+        if(n % 2 == 0){ return myPow(x * x, n / 2); }
+        else{ return x * myPow(x * x, n / 2); }
     }
 };
+
+
+// Iterative Approach
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         long nn = n;
+//         if(n < 0) { nn = -1 * nn; }
+//         double ans = 1.0;
+//         while(nn) {
+//             if(nn % 2) {
+//                 ans = ans * x;
+//                 nn = nn - 1;
+//             }
+//             else {
+//                 x = x * x;
+//                 nn = nn / 2;
+//             }
+//         }
+//         if(n < 0) { ans = 1.0 / ans; }
+//         return ans;
+//     }
+// };
 
 // Why is this not working??
 // class Solution {
