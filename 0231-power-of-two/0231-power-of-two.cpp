84efe454fcf0f0b43627f2d35396f062163e8e73
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-10-26 13:10:03 +0530

    Time: 3 ms (64.75%), Space: 5.7 MB (89.20%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        while(n > 1) {
-            if(n % 2) { return false; }
-            n /= 2;
-        }
-        return n == 1;
+        if(n <= 1) { return n == 1; }
+        if(n % 2) { return false; }
+        return isPowerOfTwo(n / 2);
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         while(n > 1) {
+//             if(n % 2) { return false; }
+//             n /= 2;
+//         }
+//         return n == 1;
+//     }
+// };
 
 // class Solution {
 // public:
@@ -22,14 +32,14 @@ public:
 //         return false;
 //     }
     
-     // int num = 1;
-        // for(int i = 0; i <= 30; i++) {
-        //     if(num == n) {
-        //         return true;
-        //     }
-        //     if(num < INT_MAX/2) {
-        //         num = num * 2;
-        //     }
-        // }
-        // return false;
-// };
+// int num = 1;
+//for(int i = 0; i <= 30; i++) {
+//     if(num == n) {
+//        return true;
+//     }
+//     if(num < INT_MAX/2) {
+//         num = num * 2;
+//     }
+// }
+// return false;
+// }
