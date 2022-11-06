a35e6b0baaabd5c0a9d679e972e0b4634d971f1e
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-11-06 20:33:47 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -5,19 +5,15 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-public:
-    void summer(int ind, vector<int>&ds, vector<int>&ans, vector<int>&arr, int N) {
+    public:
+    void summer(int ind, int sum, vector<int>&ans, vector<int>&arr, int N) {
         if(ind == N) {
-            int sum = 0;
-            for(int i: ds) { sum += i; }
             ans.push_back(sum);
             return;
         }
-        ds.push_back(arr[ind]);
-        summer(ind + 1, ds, ans, arr, N);
+        summer(ind + 1, sum + arr[ind], ans, arr, N);
         
-        ds.pop_back();
-        summer(ind + 1, ds, ans, arr, N);
+        summer(ind + 1, sum, ans, arr, N);
     }
     
 public:
@@ -25,10 +21,35 @@ public:
     {
         // Write Your Code here
         vector<int> ans;
-        vector<int> ds;
-        summer(0, ds, ans, arr, N);
+        summer(0, 0, ans, arr, N);
         return ans;
     }
+
+// public:
+//     void summer(int ind, vector<int>&ds, vector<int>&ans, vector<int>&arr, int N) {
+//         if(ind == N) {
+//             int sum = 0;
+//             for(int i: ds) { sum += i; }
+//             ans.push_back(sum);
+//             return;
+//         }
+//         ds.push_back(arr[ind]);
+//         summer(ind + 1, ds, ans, arr, N);
+        
+//         ds.pop_back();
+//         summer(ind + 1, ds, ans, arr, N);
+//     }
+    
+// public:
+//     vector<int> subsetSums(vector<int> arr, int N)
+//     {
+//         // Write Your Code here
+//         vector<int> ans;
+//         vector<int> ds;
+//         summer(0, ds, ans, arr, N);
+//         return ans;
+//     }
+
 };
 
 //{ Driver Code Starts.
