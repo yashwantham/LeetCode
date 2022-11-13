6ef168535dc22d5686516ea450de96b5432b6933
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-11-13 20:04:17 +0530

    Time: 21 ms (91.42%), Space: 16.9 MB (86.40%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,46 @@
+//Optimal Approach
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x = 0;
+        for(int i: nums) { x ^= i; }
+        return x;
+    }
+};
+
+
+
+// Approach 1
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int single = nums[0];
+//         for(int i = 1; i < nums.size(); i++) {
+//             if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+//                 single = nums[i];
+//             }
+//         }
+//         return single;
+//     }
+// };
+
+
+
+// Approach 2
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int i;
+//         for(i = 0; i < nums.size() - 1; i += 2) {
+//             if(nums[i] != nums[i + 1]) {
+//                 return nums[i];
+//             }
+//         }
+//         return nums[nums.size() - 1];
+//     }
+// };
+
+
+
