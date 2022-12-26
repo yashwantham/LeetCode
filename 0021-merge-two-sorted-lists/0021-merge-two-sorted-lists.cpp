a2f52b6fcab39a44e030026e3086afdc124bc5ed
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-12-26 17:16:01 +0530

    Time: 0 ms (100.00%), Space: 14.8 MB (82.70%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -18,26 +18,26 @@ public:
         ListNode *fh = NULL;
         ListNode *ft = NULL;
         if(list1 -> val < list2 -> val) {
-                    fh = list1;
-                    ft = list1;
-                    list1 = list1 -> next;
-                }
-                else {
-                    fh = list2;
-                    ft = list2;
-                    list2 = list2 -> next;
-                }
+            fh = list1;
+            ft = list1;
+            list1 = list1 -> next;
+        }
+        else {
+            fh = list2;
+            ft = list2;
+            list2 = list2 -> next;
+        }
         while(list1 != NULL && list2 != NULL) {
-                if(list1 -> val < list2 -> val) {
-                    ft -> next = list1;
-                    ft = list1;
-                    list1 = list1 -> next;
-                }
-                else {
-                    ft -> next = list2;
-                    ft = list2;
-                    list2 = list2 -> next;
-                }
+            if(list1 -> val < list2 -> val) {
+                ft -> next = list1;
+                ft = list1;
+                list1 = list1 -> next;
+            }
+            else {
+                ft -> next = list2;
+                ft = list2;
+                list2 = list2 -> next;
+            }
         }
         if(list1 != NULL) {
             ft -> next = list1;
