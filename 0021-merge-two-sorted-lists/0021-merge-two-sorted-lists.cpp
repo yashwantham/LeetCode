9b8ac4c2d86e7392f956a3eb03e70948b27b1b54
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2022-12-26 18:13:39 +0530

    Time: 12 ms (62.95%), Space: 14.9 MB (44.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,45 +8,63 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-// Iterative
+//Recursive
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1 == NULL) { return list2; }
         if(list2 == NULL) { return list1; }
-        ListNode *fh = NULL;
-        ListNode *ft = NULL;
-        if(list1 -> val < list2 -> val) {
-            fh = list1;
-            ft = list1;
-            list1 = list1 -> next;
+        if(list1 -> val <= list2 -> val) {
+            list1 -> next = mergeTwoLists(list1 -> next, list2);
+            return list1;
         }
         else {
-            fh = list2;
-            ft = list2;
-            list2 = list2 -> next;
+            list2 -> next = mergeTwoLists(list1, list2 -> next);
+            return list2;
         }
-        while(list1 != NULL && list2 != NULL) {
-            if(list1 -> val < list2 -> val) {
-                ft -> next = list1;
-                ft = list1;
-                list1 = list1 -> next;
-            }
-            else {
-                ft -> next = list2;
-                ft = list2;
-                list2 = list2 -> next;
-            }
-        }
-        if(list1 != NULL) {
-            ft -> next = list1;
-            return fh;
-        }
-        if(list2 != NULL) {
-            ft -> next = list2;
-            return fh;
-        }
-        return NULL;
     }
 };
+
+
+
+// Iterative
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1 == NULL) { return list2; }
+//         if(list2 == NULL) { return list1; }
+//         ListNode *fh = NULL;
+//         ListNode *ft = NULL;
+//         if(list1 -> val < list2 -> val) {
+//             fh = list1;
+//             ft = list1;
+//             list1 = list1 -> next;
+//         }
+//         else {
+//             fh = list2;
+//             ft = list2;
+//             list2 = list2 -> next;
+//         }
+//         while(list1 != NULL && list2 != NULL) {
+//             if(list1 -> val < list2 -> val) {
+//                 ft -> next = list1;
+//                 ft = list1;
+//                 list1 = list1 -> next;
+//             }
+//             else {
+//                 ft -> next = list2;
+//                 ft = list2;
+//                 list2 = list2 -> next;
+//             }
+//         }
+//         if(list1 != NULL) {
+//             ft -> next = list1;
+//             return fh;
+//         }
+//         if(list2 != NULL) {
+//             ft -> next = list2;
+//             return fh;
+//         }
+//         return fh;
+//     }
+// };
