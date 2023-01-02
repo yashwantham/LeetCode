c808dfc55852b2e62c846b0a01d5829679e11a76
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2023-01-03 02:21:08 +0530

    Time: 3 ms (98.28%), Space: 8.5 MB (19.05%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,16 +10,52 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head -> next == NULL) { return head; }
-        
-        ListNode* smallAns = reverseList(head -> next);
-        ListNode *temp = smallAns;
-        while(temp -> next != NULL) {
-            temp = temp -> next;
+    class Pair {
+        public:
+            ListNode *head;
+            ListNode *tail;
+    };
+    
+    Pair reverseList2(ListNode* head) {
+        if(head == NULL || head -> next == NULL) {
+            Pair ans;
+            ans.head = head;
+            ans.tail = head;
+            return ans;
         }
-        temp -> next = head;
+        
+        Pair smallAns = reverseList2(head -> next);
+        smallAns.tail -> next = head;
         head -> next = NULL;
+        smallAns.tail = head;
+        
         return smallAns;
     }
+    
+    ListNode* reverseList(ListNode* head) {
+        return reverseList2(head).head;
+    }
 };
+
+
+
+
+
+
+
+// Recursive approach  TC: O(n²)
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head == NULL || head -> next == NULL) { return head; }
+        
+//         ListNode* smallAns = reverseList(head -> next);
+//         ListNode *temp = smallAns;
+//         while(temp -> next != NULL) {
+//             temp = temp -> next;
+//         }
+//         temp -> next = head;
+//         head -> next = NULL;
+//         return smallAns;
+//     }
+// };
