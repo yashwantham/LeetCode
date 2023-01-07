7fbd7089d18b1cfd232e9e51469617d2c7175b1f
Yashwanth A M <68170694+yashwantham@users.noreply.github.com>
2023-01-08 00:48:32 +0530

    Time: 185 ms (95.77%), Space: 76.4 MB (76.65%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int count = 0;
+        for(int i: costs) {
+            if(coins >= i) {
+                count++; 
+                coins -= i;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+// this is not optimal solution
+
+// try to learn using count sort 
